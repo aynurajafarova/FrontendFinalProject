8ce93ae1bcd407519c67ceedaf4c2fc99ffb663b
aynurajafarova <aynurafj@code.edu.az>
2019-09-03 18:24:40 -0700

    magnificPopup folder was added to the js folder

---
+++ assets/js/magnificPopup/popup.js
@@ -0,0 +1,7 @@
+$('.single-blog-post-images').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+        enabled: true
+    }
+});

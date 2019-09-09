42bf81b20fcac69e96e9d17e43f8b485ca1bf6bf
aynurajafarova <aynurafj@code.edu.az>
2019-09-08 22:27:02 -0700

    comments were added to the smoothScroll.js file

--- assets/js/smoothScroll.js
+++ assets/js/smoothScroll.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
-    $('.submenu a').on('click', function (e) { 
-        if (this.hash !== '') {
-            e.preventDefault();
+    $('.submenu a').on('click', function (e) {  // Adding click event
+        if (this.hash !== '') { // if hash is not equal to an empty string
+            e.preventDefault();  // Prevent default behavior
 
-            const hash = this.hash;
-            $('html,body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800);
+            const hash = this.hash;  // Putting a hash value into a variable
+            $('html,body').animate({  // Target the body and call the animate method and inside animate it's going to take an object
+                scrollTop: $(hash).offset().top  // taking the hash value and calling offset method and it will scroll down from the top
+            }, 800);  // speed that we want to scroll, milliseconds
         };
     })
 
